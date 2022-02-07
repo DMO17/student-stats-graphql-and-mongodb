e322c72a138cd44d32b1606ba85b4406ca3de3c7
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-07 11:10:44 +0000

    connection established with sandbox

--- src/resolvers/course.js
+++ src/resolvers/course.js
@@ -1,5 +1,34 @@
-const courses = () => {};
-const course = () => {};
+const courses = () => {
+  return [
+    {
+      courseName: "Full Stack Coding Bootcamp",
+      inPerson: false,
+      students: [],
+    },
+    {
+      courseName: "AI Coding Bootcamp",
+      inPerson: true,
+      students: [],
+    },
+    {
+      courseName: "MatLab and Simulink",
+      inPerson: true,
+      students: [],
+    },
+    {
+      courseName: "3D CAD DRAWING",
+      inPerson: true,
+      students: [],
+    },
+  ];
+};
+const course = () => {
+  return {
+    courseName: "Full Stack Coding Bootcamp",
+    inPerson: false,
+    students: [],
+  };
+};
 const createCourse = () => {};
 
 module.exports = { courses, course, createCourse };
--- src/resolvers/student.js
+++ src/resolvers/student.js
@@ -1,5 +1,108 @@
-const students = () => {};
-const student = () => {};
+const students = () => {
+  return [
+    {
+      firstName: "Bob",
+      lastName: "Smith",
+      githubUrl: "https://github.com/bobsmith",
+      assignments: [
+        { assignmentName: "HTML Intro", score: 90 },
+        {
+          assignmentName: "CSS Intro",
+          score: 95,
+        },
+        {
+          assignmentName: "JS Intro",
+          score: 60,
+        },
+      ],
+    },
+    {
+      firstName: "Jane",
+      lastName: "Smith",
+      githubUrl: "https://github.com/janesmith",
+      assignments: [
+        {
+          assignmentName: "HTML Intro",
+          score: 95,
+        },
+        {
+          assignmentName: "CSS Intro",
+          score: 95,
+        },
+        {
+          assignmentName: "JS Intro",
+          score: 80,
+        },
+      ],
+    },
+    {
+      firstName: "Jack",
+      lastName: "Smith",
+      githubUrl: "https://github.com/jacksmith",
+      assignments: [
+        { assignmentName: "HTML Intro", score: 40 },
+        {
+          assignmentName: "CSS Intro",
+          score: 45,
+        },
+        {
+          assignmentName: "JS Intro",
+          score: 50,
+        },
+      ],
+    },
+    {
+      firstName: "Tom",
+      lastName: "Smith",
+      githubUrl: "https://github.com/tomsmith",
+      assignments: [
+        { assignmentName: "HTML Intro", score: 85 },
+        {
+          assignmentName: "CSS Intro",
+          score: 64,
+        },
+        {
+          assignmentName: "JS Intro",
+          score: 66,
+        },
+      ],
+    },
+    {
+      firstName: "Jill",
+      lastName: "Smith",
+      githubUrl: "https://github.com/jillsmith",
+      assignments: [
+        { assignmentName: "HTML Intro", score: 60 },
+        {
+          assignmentName: "CSS Intro",
+          score: 60,
+        },
+        {
+          assignmentName: "JS Intro",
+          score: 60,
+        },
+      ],
+    },
+  ];
+};
+const student = () => {
+  return {
+    firstName: "Bob",
+    lastName: "Smith",
+    githubUrl: "https://github.com/bobsmith",
+    assignments: [
+      { assignmentName: "HTML Intro", score: 90 },
+      {
+        assignmentName: "CSS Intro",
+        score: 95,
+      },
+      {
+        assignmentName: "JS Intro",
+        score: 60,
+      },
+    ],
+  };
+};
 const createStudent = () => {};
 
 module.exports = { students, student, createStudent };
